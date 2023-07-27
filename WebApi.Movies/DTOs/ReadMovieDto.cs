02f316a6bf3576b8df676c9336a69329c52c6d6d
Luís Felipe <luisrodrigues@bne.com.br>
2023-07-27 17:07:31 -0300

    Criação dos DTOs da entidade Movie

---
+++ WebApi.Movies/DTOs/ReadMovieDto.cs
@@ -0,0 +1,4 @@
+﻿namespace WebApi.Movies.DTOs
+{
+    public record ReadMovieDto(Guid Id, string Title, string Summary, string Genre, int Year, int DurationInMinutes, double Rating);
+}
---
+++ WebApi.Movies/DTOs/UpdateMovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Movies.DTOs
+{
+    public record UpdateMovieDto
+    {
+        [Required(ErrorMessage = $"O campo {nameof(Title)} é obrigatório.")]
+        [StringLength(75, ErrorMessage = $"O campo {nameof(Title)} precisa conter no máximo 75 caracteres.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = $"O campo {nameof(Summary)} é obrigatório.")]
+        [StringLength(200, ErrorMessage = $"O campo {nameof(Summary)} precisa conter no máximo 200 caracteres.")]
+        public string Summary { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = $"O campo {nameof(Genre)} é obrigatório.")]
+        [StringLength(15, ErrorMessage = $"O campo {nameof(Genre)} precisa conter no máximo 15 caracteres.")]
+        public string Genre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = $"O campo {nameof(Year)} é obrigatório.")]
+        [Range(4, 4, ErrorMessage = $"O campo {nameof(Year)} precisa conter exatamente 4 caracteres.")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = $"O campo {nameof(DurationInMinutes)} é obrigatório.")]
+        [Range(2, 3, ErrorMessage = $"O campo {nameof(DurationInMinutes)} precisa conter de 2 a 3 caracteres.")]
+        public int DurationInMinutes { get; set; }
+
+        [Required(ErrorMessage = $"O campo {nameof(Rating)} é obrigatório.")]
+        public double Rating { get; set; }
+    }
+}
